e4854c3ac3d56c58cad29eff098b6ff80b3ad551
nicholasrchiarulli@gmail.com <nicholaschiarulli>
2019-05-20 15:11:02 -0400

    dashboard

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,10 @@ import Landing from "./components/layout/Landing";
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
 import Alert from "./components/layout/Alert";
-
+import Dashboard from "./components/dashboard/Dashboard";
+import PrivateRoute from "./components/routing/PrivateRoute";
+import CreateProfile from "./components/profile-forms/CreateProfile";
+import EditProfile from "./components/profile-forms/EditProfile";
 // Redux
 import { Provider } from "react-redux";
 import store from "./store";
@@ -31,7 +34,18 @@ const App = () => {
             <Alert />
             <Switch>
               <Route exact path="/register" component={Register} />
-              <Route exact path="/Login" component={Login} />
+              <Route exact path="/login" component={Login} />
+              <PrivateRoute exact path="/dashboard" component={Dashboard} />
+              <PrivateRoute
+                exact
+                path="/create-profile"
+                component={CreateProfile}
+              />
+              <PrivateRoute
+                exact
+                path="/edit-profile"
+                component={EditProfile}
+              />
             </Switch>
           </section>
         </Fragment>
